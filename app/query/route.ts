8698c01cc72f6ad5c570961834092f4c2d967e17
yohanneskf <yohanneskifle540@gmail.com>
2025-07-26 20:43:17 +0300

    commit3

--- app/query/route.ts
+++ app/query/route.ts
@@ -13,14 +13,14 @@ async function listInvoices() {
   return data;
 }
 
-// export async function GET() {
-//   return Response.json({
-//     message:
-//       "Uncomment this file and remove this line. You can delete this file when you are finished.",
-//   });
-//   try {
-//     return Response.json(await listInvoices());
-//   } catch (error) {
-//     return Response.json({ error }, { status: 500 });
-//   }
-// }
+export async function GET() {
+  return Response.json({
+    message:
+      "Uncomment this file and remove this line. You can delete this file when you are finished.",
+  });
+  try {
+    return Response.json(await listInvoices());
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
