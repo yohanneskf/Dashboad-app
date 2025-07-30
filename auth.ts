c76d376d746eff511a83c031754ed9131704b689
yohanneskf <yohanneskifle540@gmail.com>
2025-07-30 16:57:12 +0300

    e1

--- auth.ts
+++ auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
-import Credentials from "next-auth/providers/credentials";
 import { authConfig } from "./auth.config";
+import Credentials from "next-auth/providers/credentials";
 import { z } from "zod";
 import type { User } from "@/app/lib/definitions";
 import bcrypt from "bcrypt";
@@ -31,9 +31,12 @@ export const { auth, signIn, signOut } = NextAuth({
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
           if (!user) return null;
+
           const passwordsMatch = await bcrypt.compare(password, user.password);
+
           if (passwordsMatch) return user;
         }
+
         console.log("Invalid credentials");
         return null;
       },
